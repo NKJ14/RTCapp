abe8703f4f7cbb1406b47f0fac77d1a9b568a738
NKJ14 <nishanthere1402@gmail.com>
2025-01-22 01:42:37 +0530

    updated messaging service's logic.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let senderSocket: null | WebSocket = null;
+let senderSocket:null | WebSocket = null;
 let receiverSocket: null | WebSocket = null;
 
 wss.on('connection', function connection(ws) {
@@ -10,8 +10,20 @@ wss.on('connection', function connection(ws) {
 
   ws.on('message', function message(data: any) {
     const message = JSON.parse(data);
+    // create offer
+    //create answer
+    //add ICE candidates
+    //sender-identifies-as-sender
+    //receiver-identifies-as-receiver
+
+    if(message.type ==="identify-as-sender"){
+        senderSocket = ws;
+    } else if(message.type==="identify-as-receiver"){
+        receiverSocket = ws;
+    } else if(message.type==="create-offer"){
+        receiverSocket?.send(JSON.stringify({type:"offer", offer:message.offer}))
+    }
+
     console.log(message);
   });
-  
-  ws.send('something');
 });
